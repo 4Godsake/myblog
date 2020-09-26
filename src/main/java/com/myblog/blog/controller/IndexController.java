60b5b6b765cb5722460348070f3bf7c7fae5a846
4Godsake <1025744898@qq.com>
2020-09-27 02:07:25 +0800

    update blog

--- src/main/java/com/myblog/blog/controller/IndexController.java
+++ src/main/java/com/myblog/blog/controller/IndexController.java
@@ -31,7 +31,7 @@ public class IndexController {
     private CommentService commentService;
 
     @GetMapping("/")
-    public String index(@PageableDefault(size = 10,sort = {"updateTime"},direction = Sort.Direction.DESC)
+    public String index(@PageableDefault(size = 3,sort = {"updateTime"},direction = Sort.Direction.DESC)
                                     Pageable pageable, Model model){
         model.addAttribute("page",blogService.listBlog(pageable));
         model.addAttribute("catalogs",catalogService.listCatalogTop(6));
